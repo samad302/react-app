026503eb217aa31b5c3569fb5b21576b6604b9d7
samad302 <sammadjawaid465@gmail.com>
2024-03-17 04:02:00 +0500

    first commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -23,7 +23,10 @@ export default function Banner() {
              
 
             </div>
-            <button className='bg-black text-white w-[200px] rounded-md font-medium  mx-auto md:mx-0 py-3'>Get Started</button>
+            <button class="bg-[#2699fb] relative h-12 w-40 overflow-hidden border border-indigo-600 text-white -600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-[#b3d4f1] hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
+      <span class="relative z-10">Center Hover</span>
+    </button>
+            {/* <button className='bg-[#2699fb] text-white w-[200px] rounded-md font-medium  mx-auto md:mx-0 py-3'>Get Started</button> */}
 
         </div>
     </div>
